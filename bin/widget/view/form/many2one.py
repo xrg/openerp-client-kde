fd5cd677db8a98600db580d0c6e4adc9d802b8f9
albertcervera <>
2008-03-04 19:54:52 +0000

    Cleanup many2one load completion.

--- bin/widget/view/form/many2one.py
+++ bin/widget/view/form/many2one.py
@@ -136,21 +136,16 @@ class ManyToOneFormWidget(AbstractFormWidget):
 		self.completion = QCompleter()
 		self.completion.setCaseSensitivity( Qt.CaseInsensitive )
 		self.uiText.setCompleter( self.completion )
-		self.liststore = QStringListModel()
 		liststore = []
-		liststore2 = []
 		for i,word in enumerate( ids ):
 			if word[1][0] == '[':
 				i = word[1].find( ']')
 				s = word[1][1:i]
 				s2 = word[1][i+2:]
-				liststore2.append( [ ( str(s) + " " + str( s2 ), s2 )])
 				liststore.append( s2 )
 			else:
-				liststore2.append( [ word[1],word[1] ])
-				liststore.append( word[1])
-		self.liststore.setStringList( liststore )
-		self.completion.setModel( self.liststore )
+				liststore.append( word[1] )
+		self.completion.setModel( QStringListModel( liststore ) )
 		self.completion.setCompletionColumn( 0 )
 
 	def setReadOnly(self, value):
