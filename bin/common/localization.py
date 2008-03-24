164fb21afc2e272fe4ececea8d55d740f9ed84c6
albertcervera <>
2008-03-24 19:31:25 +0000

    Try to be smarter when loading language files.

--- bin/common/localization.py
+++ bin/common/localization.py
@@ -15,6 +15,19 @@ def initializeTranslations():
 def initializeQtTranslations():
 	from PyQt4.QtCore import QTranslator, QCoreApplication, QLocale
 	translator = QTranslator( QCoreApplication.instance() )
-	translator.load( paths.searchFile( str(QLocale.system().name()) , 'l10n' ) )
+	language = str(QLocale.system().name())
+	# First we try to load the file with the same system language name 
+	# Usually in $LANG and looks something like ca_ES, de_DE, etc.
+	file = paths.searchFile( language, 'l10n' )
+	if not file:
+		# If the first step didn't work try to remove country
+		# information and try again.
+		language = language.split('_')[0]
+		file = paths.searchFile( language, 'l10n' )
+	if not file:
+		# If no translation files were found, don't crash
+		# but continue silently.
+		return
+	translator.load( file )
 	QCoreApplication.instance().installTranslator( translator )
 
