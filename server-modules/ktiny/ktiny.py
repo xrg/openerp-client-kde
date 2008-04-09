5e0a7f2084efccb028bbbe0ad0d515cc462c1575
albertcervera <>
2008-04-09 22:12:59 +0000

    Added roles relationship.

--- server-modules/ktiny/ktiny.py
+++ server-modules/ktiny/ktiny.py
@@ -9,6 +9,13 @@ class ir_attachment(osv.osv):
 	}
 ir_attachment()
 
+class res_roles(osv.osv):
+	_name = 'res.roles'
+	_inherit = 'res.roles'
+	_columns = {
+		'ktiny_settings_id': fields.many2many('nan.ktiny.settings', 'nan_ktiny_settings_roles_rel', 'role_id', 'setting_id', 'KTiny Settings')
+	}
+
 class nan_ktiny_settings(osv.osv):
 	_name = 'nan.ktiny.settings'
 
@@ -17,8 +24,30 @@ class nan_ktiny_settings(osv.osv):
 		'show_toolbar': fields.boolean( 'Show toolbar' ),
 		'tabs_position': fields.selection( [('left', 'Left'), ('top', 'Top'), 
 			('right', 'Right'), ('bottom', 'Bottom')], 'Default tabs position' ),
-		'stylesheet': fields.text( 'Stylesheet' )
+		'stylesheet': fields.text( 'Stylesheet' ),
+		'roles_id': fields.many2many('res.roles', 'nan_ktiny_settings_roles_rel', 'setting_id', 'role_id', 'Roles')
 	}
+
+	# Returns the id of the settings to load. Currently only uses roles to
+	# decide the appropiate settings, but in the future it could use user IP
+	# address or other supplied parameters.
+	def get_settings_id(self, cr, uid):
+		user = self.pool.get('res.users').browse(cr, uid, [uid])[0]
+		ids = [x.id for x in user.roles_id]
+		ids = str(ids).replace('[', '(').replace(']',')')
+		cr.execute( "SELECT setting_id FROM nan_ktiny_settings_roles_rel WHERE role_id IN %s" % ( ids ) )
+		row = cr.fetchone()
+		if row:
+			return row[0]
+		else:
+			return False
+
+	def get_settings(self, cr, uid):
+		id = self.get_settings_id(cr, uid)
+		if not id:
+			return {}
+		return self.read(cr, uid, [id])
+		
 
 nan_ktiny_settings()
 
