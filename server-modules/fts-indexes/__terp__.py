f99f7d19ef3b139cf0db840ef41232079bf71277
albertcervera <>
2008-08-10 23:40:26 +0000

    Added fts-indexes module that simply adds a bunch of fields to the list of fields to be indexed by the full_text_search module.

---
+++ server-modules/fts-indexes/__terp__.py
@@ -0,0 +1,13 @@
+{
+		"name" : "fts-indexes",
+		"version" : "1.0",
+		"author" : "NaN",
+		"website" : "http://ktiny.sf.net",
+		"category" : "Vertical Modules/Parametrization",
+		"description": """This module adds some indexes for the full text search module.""",
+		"depends" : ["full_text_search", "smart_attach"],
+		"init_xml" : [ ],
+		"demo_xml" : [ ],
+		"update_xml" : ["fts-indexes_data.xml"],
+		"installable": True
+} 
