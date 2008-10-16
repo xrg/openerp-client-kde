453585a18656cb1366e9892300713c1e048fd447
Albert Cervera i Areny <albert@nan-tic.com>
2008-10-17 01:11:12 +0200

    Add "product" and "base" modules dependencies in fts-indexes module.

--- server-modules/fts-indexes/__terp__.py
+++ server-modules/fts-indexes/__terp__.py
@@ -32,7 +32,7 @@
 		"website" : "http://ktiny.sf.net",
 		"category" : "Vertical Modules/Parametrization",
 		"description": """This module adds some indexes for the full text search module.""",
-		"depends" : ["full_text_search", "smart_attach"],
+		"depends" : ["base", "product", "full_text_search", "smart_attach"],
 		"init_xml" : [ ],
 		"demo_xml" : [ ],
 		"update_xml" : ["fts-indexes_data.xml"],
