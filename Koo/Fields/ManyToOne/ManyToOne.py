49225be9cee25f12c3e26572a48d7ad8727acc8f
Albert Cervera i Areny <albert@nan-tic.com>
2009-01-11 16:46:20 +0100

    Fix relate actions in ManyToOne widgets.

--- Koo/Fields/ManyToOne/ManyToOne.py
+++ Koo/Fields/ManyToOne/ManyToOne.py
@@ -269,14 +269,13 @@ class ManyToOneFieldWidget(AbstractFieldWidget, ManyToOneFieldWidgetUi):
 		return currentEntries
 
 	def executeRelation(self, action):
-		value = self.model.value(self.name)
+		id = self.model.get()[self.name]
 		group = ModelRecordGroup( self.attrs['relation'] )
-		group.load( [value] )
+		group.load( [id] )
 		record = group.modelByRow( 0 )
 		action['domain'] = record.evaluateExpression( action['domain'], check_load=False)
 		action['context'] = str( record.evaluateExpression( action['context'], check_load=False) )
 		Api.instance.executeAction( action )
-		return 
 
 	def executeAction(self, type):
 		id = self.model.id
