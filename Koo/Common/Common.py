4eafd0a13c3fa3e9d979072092b7de064301b851
P. Christeas <p_christ@hol.gr>
2009-10-30 19:29:40 +0200

    Fix Error-in-error logic
    Last line was lost in some merge, so only one error appeared per session.

--- Koo/Common/Common.py
+++ Koo/Common/Common.py
@@ -187,6 +187,7 @@ def error(title, message, details=''):
 	dialog.exec_()
 	errorInError=False
 	QApplication.restoreOverrideCursor()
+	errorInError=False
 
 (ProgressDialogUi, ProgressDialogBase) = loadUiType( uiPath('progress.ui') )
 		
