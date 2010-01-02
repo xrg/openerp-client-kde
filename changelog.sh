76d24574a5722761df885d8a710fbfa249a8078c
Albert Cervera i Areny <albert@nan-tic.com>
2010-01-02 18:23:29 +0100

    Improved changelog.sh script to create valid .rst output.

--- changelog.sh
+++ changelog.sh
@@ -1,20 +1,47 @@
 #!/bin/bash
 
-last_tag=$(bzr tags | head -1)
-tag=$(echo "$last_tag" | awk '{print $1}')
-rev=$(echo "$last_tag" | awk '{print $2}')
 
-echo "Changelog since $tag (r$rev):"
-log=$(bzr log -r$rev.. | grep -v "^revno: " | grep -v "^committer: " | grep -v "^branch nick: " | grep -v "^timestamp: " | grep -v "^----------------------" | grep -v "^message:" | sed 's/^  /-/g')
+function print_log()
+{
+	current_tag=$1
+	current_rev=$2
+	previous_tag=$3
+	previous_rev=$4
 
-echo "Koo Fixes:"
-echo "$log" | grep -iv 'jasper' | grep -i 'fix'
-echo
-echo "Koo Improvements:"
-echo "$log" | grep -iv 'jasper' | grep -iv 'fix'
-echo
-echo "Jasper Reports Fixes:"
-echo "$log" | grep -i 'jasper' | grep -i 'fix'
-echo
-echo "Jasper Reports Improvements:"
-echo "$log" | grep -i 'jasper' | grep -iv 'fix'
+	log=$(bzr log -r$previous_rev..$current_rev | grep -v "^revno: " | grep -v "^committer: " | grep -v "^branch nick: " | grep -v "^timestamp: " | grep -v "^----------------------" | grep -v "^message:" | grep -v "^tags:" | sed 's/^  /- /g')
+
+	log=$(echo "$log" | sed 's_\#\(.\+\)_https://bugs.launchpad.net/openobject-client-kde/+bug/\1_g')
+
+	title="Changelog from $previous_tag (r$previous_rev) to $current_tag (r$current_rev):"
+	echo $title
+	printf "%${#title}s\n" | sed 's/ /=/g'
+	echo
+	echo "Koo Fixes:"
+	echo "----------"
+	echo "$log" | grep -iv 'jasper' | grep -i 'fix'
+	echo
+	echo "Koo Improvements:"
+	echo "-----------------"
+	echo "$log" | grep -iv 'jasper' | grep -iv 'fix'
+	echo
+	echo "Jasper Reports Fixes:"
+	echo "---------------------"
+	echo "$log" | grep -i 'jasper' | grep -i 'fix'
+	echo
+	echo "Jasper Reports Improvements:"
+	echo "----------------------------"
+	echo "$log" | grep -i 'jasper' | grep -iv 'fix'
+}
+
+all_tags=$(bzr tags)
+
+current_tag='trunk'
+current_rev=$(bzr log -l1 | grep "^revno: " | cut -d " " -f 2)
+for current in $(echo "$all_tags" | sed 's/ \+/#/g'); do
+	previous_tag=$(echo "$current" | cut -d '#' -f 1)
+	previous_rev=$(echo "$current" | cut -d '#' -f 2)
+	print_log $current_tag $current_rev $previous_tag $previous_rev
+	current_tag=$previous_tag
+	current_rev=$previous_rev
+	exit 0
+done
