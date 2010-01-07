e0fe839466fd3d3657ba149fc3e7da0cc2f44bad
Albert Cervera i Areny (albert@nan-tic.com) <>
2010-01-07 21:37:28 +0100

    Ensure the version of python used to create the installer is 2.6 in case there are other versions in the system.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 rmdir dist /s /q
 rmdir build /s /q
-setup.py py2exe
+c:\python26\python setup.py py2exe
 mkdir dist\imageformats
 copy c:\python26\lib\site-packages\PyQt4\plugins\imageformats\qjpeg4.dll dist\imageformats\
 cd nsis
