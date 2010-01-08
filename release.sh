08a8afa5394ae59906e037e414b4081b3af8ceac
Albert Cervera i Areny <albert@nan-tic.com>
2010-01-08 09:44:15 +0100

    Improved release.sh: Create .tar.bz2 package.

--- release.sh
+++ release.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ "$1" == "" ]; then
+	echo "Usage: $0 version"
+	exit 1
+fi
+
+echo "Ensure you've updated the version in debian/changelog file."
+read
+
 make builddeb
 
 pushd server-modules
@@ -12,3 +20,4 @@ mkdir release
 mv server-modules/*.zip release
 mv ../koo*deb release
 
+tar jcvf release/koo-$version.tar.bz2 Koo
