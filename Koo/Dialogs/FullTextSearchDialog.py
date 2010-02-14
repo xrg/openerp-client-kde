740e0c02f2ea07a0fb7202cca4290f6366c5022a
Albert Cervera i Areny <albert@nan-tic.com>
2010-02-14 11:47:56 +0100

    [REF] Changed the way WebKit is loaded in FullTextSearchDialog so it properly works on Windows. Patch by Luigi Grilli.

--- Koo/Dialogs/FullTextSearchDialog.py
+++ Koo/Dialogs/FullTextSearchDialog.py
@@ -40,6 +40,7 @@ from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 from PyQt4.uic import *
 from PyQt4.QtWebKit import *
+from PyQt4.QtNetwork import *
 
 
 (FullTextSearchDialogUi, FullTextSearchDialogBase) = loadUiType( Common.uiPath('full_text_search.ui') )
