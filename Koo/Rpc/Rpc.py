dfef35f1f9344c72dfee2948ef1987305385f40f
P. Christeas <p_christ@hol.gr>
2010-05-11 03:00:37 +0300

    Update copyright notices

--- Koo/Rpc/Rpc.py
+++ Koo/Rpc/Rpc.py
@@ -1,7 +1,8 @@
 ##############################################################################
 #
 # Copyright (c) 2004-2006 TINY SPRL. (http://tiny.be) All Rights Reserved.
-# Copyright (c) 2007-2008 Albert Cervera i Areny <albert@nan-tic.com>
+# Copyright (c) 2007-2010 Albert Cervera i Areny <albert@nan-tic.com>
+# Copyright (c) 2010 P. Christeas <p_christ@hol.gr>
 #
 # WARNING: This program as such is intended to be used by professional
 # programmers who take the whole responsability of assessing all potential
