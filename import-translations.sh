cc9f5f8ee0112efb6e0ee285d2778a5062809fb1
Albert Cervera i Areny <albert@nan-tic.com>
2010-05-12 04:24:04 +0200

    [FIX] Fixed import-translations.sh script, search po2ts.py at the correct directory.

--- import-translations.sh
+++ import-translations.sh
@@ -5,6 +5,8 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
+dir=$(pwd)
+
 pushd Koo/l10n
 
 url="$1"
@@ -30,7 +32,7 @@ for file in $(find -iname "qt-koo-*.po"); do
 	lang=$(echo $file | cut -d "-" -f 3 | cut -d "." -f 1)
 	cp $file $appdir/qt-koo-$lang.po
 	#lconvert $appdir/qt-koo-$lang.po -o $appdir/$lang.ts
-	./po2ts.py $appdir/qt-koo-$lang.po -o $appdir/$lang.ts
+	$dir/po2ts.py $appdir/qt-koo-$lang.po -o $appdir/$lang.ts
 	echo "CREATED: " $appdir/$lang.ts
 done
 
