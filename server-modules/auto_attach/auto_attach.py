5ccd19310c8bbc840a49fe19097919f9d4b83975
Albert Cervera i Areny <albert@nan-tic.com>
2010-06-13 00:43:09 +0200

    [IMP] auto_attach: Added the reference with the attachment created by the process. Also create the attachment before executing the action in cas the action needs the reference to the attachment.

--- server-modules/auto_attach/auto_attach.py
+++ server-modules/auto_attach/auto_attach.py
@@ -155,6 +155,7 @@ class nan_document(osv.osv):
 		'property_ids': fields.one2many('nan.document.property', 'document_id', 'Properties', readonly=True, states=state_only_read),
 		'template_id': fields.many2one('nan.template', 'Template', readonly=True, states=state_only_read),
 		'document_id': fields.reference('Document', selection=attachableDocuments, size=128, readonly=True, states=state_only_read),
+		'attachment_id': fields.many2one('ir.attachment', 'Attachment', readonly=True, ondelete='cascade', help='Attachment created by this document.'),
 		'task' : fields.text('Task', readonly=True),
 		'state': fields.selection( [
 			('pending','Pending'),('analyzing','Analyzing'),('analyzed','Analyzed'),
@@ -468,6 +469,40 @@ class nan_document(osv.osv):
 			if not explain and document.state not in ('verified','processing'):
 				continue
 
+			if not explain and document.document_id:
+				# Attach document to the appropiate reference
+				ref = document.document_id.split(',')
+				model = ref[0]
+				id = ref[1]
+
+				# Ensure attachment name is not duplicated because there's a restriction
+				# in ir.attachment model.
+				count = 0
+				while True:
+					current_name = '%s (%d)' % (document.name, count)
+					name = count and current_name or document.name
+					ids = self.pool.get('ir.attachment').search(cr, uid, [
+						('res_id','=',id),
+						('res_model','=',model),
+						('name','=',name)
+					], context=context)
+					if not ids:
+						break
+					count += 1
+
+				attachment_id = self.pool.get('ir.attachment').create( cr, uid, { 
+					'res_id': id,
+					'res_model': model,
+					'name': name,
+					'datas': document.datas,
+					'datas_fname': document.filename or document.name,
+					'description': _('Document attached automatically'),
+				}, context)
+				self.write(cr, uid, [document.id], {
+					'state': 'processed',
+					'attachment_id': attachment_id,
+				}, context)
+
 			task = None
 			if document.template_id:
 				function = document.template_id.action_function
@@ -479,23 +514,6 @@ class nan_document(osv.osv):
 					task = eval('obj.%s(cr, uid, document.id, explain, %s, context)' % ( name, ','.join( parameters ) ) )
 			if explain:
 				self.write( cr, uid, [document.id], {'task': task} )
-			elif document.document_id:
-				# Attach document to the appropiate reference
-				ref = document.document_id.split(',')
-				model = ref[0]
-				id = ref[1]
-				self.pool.get( 'ir.attachment' ).create( cr, uid, { 
-					'res_id': id,
-					'res_model': model,
-					'name': document.name,
-					'datas': document.datas,
-					'datas_fname': document.filename or document.name,
-					'description': _('Document attached automatically'),
-				}, context)
-				self.write(cr, uid, [document.id], {
-					'state': 'processed'
-				}, context)
-
 
 	def executeAttachs( self, cr, uid, ids, context=None ):
 		if context is None:
