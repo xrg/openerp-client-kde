90472c95a81c074ad1b4bd93334b0bf48fc5d58e
P. Christeas <xrg@linux.gr>
2012-07-06 01:47:33 +0300

    Common.Icons: cache the pixmaps, log errors
    Since the server may suggest Icons yet unknown to this client, we
    have to be verbose about them. Also, should not reload the pixmap
    at each Model.data() call. With a finite dictionary of icons, we'd
    better cache them.

--- Koo/Common/Icons.py
+++ Koo/Common/Icons.py
@@ -33,108 +33,113 @@ import logging
 # GTK -> KDE Icons mapping
 # Due to the fact that the server expects the client to understand GTK icon names
 # we used a set of two functions and a dictionary to help us work correctly.
-# The 'mapIcons' dictionary should have an entry for each icon name used by 
+# The 'mapIcons' dictionary should have an entry for each icon name used by
 # the server, the value of the entry is the name of the image in our resource
-# file. Simply use kdeIcon() and kdePixmap() functions to get the appropiate 
+# file. Simply use kdeIcon() and kdePixmap() functions to get the appropiate
 # Icon or Pixmap.
 mapIcons = {
-	'gtk-apply' : 'apply.png',
-	'gtk-cancel' : 'cancel.png',
-	'gtk-close' : 'close.png',
-	'gtk-convert' : 'convert.png',
-	'gtk-dialog-info' : 'info.png',
-	'gtk-edit' : 'edit.png',
-	'gtk-execute' : 'exec.png',
-	'gtk-find' : 'find.png',
-	'gtk-go-back' : 'previous.png',
-	'gtk-go-down' : 'down.png',
-	'gtk-go-forward' : 'next.png',
-	'gtk-go-up' : 'up.png',
-	'gtk-indent': 'indent.png',
-	'gtk-info' : 'info.png',
-	'gtk-jump-to' : 'relate.png',
-	'gtk-media-next' : 'media_next.png',
-	'gtk-media-pause' : 'pause.png',
-	'gtk-media-previous' : 'media_previous.png',
-	'gtk-new' : 'new.png',
-	'gtk-ok' : 'ok.png',
-	'gtk-open' : 'open.png',
-	'gtk-print' : 'print.png',
-	'gtk-save' : 'save.png',
-	'gtk-select-all' : 'text_block.png',
-	'gtk-sort-descending' : 'down.png',
-	'gtk-undo' : 'undo.png',
-	'gtk-yes' : 'ok.png',
-	'STOCK_CONVERT' : 'convert.png',
-	'STOCK_EXECUTE' : 'action.png',
-	'STOCK_INDENT' : 'rightjust.png',
-	'STOCK_JUSTIFY_FILL' : 'text_block.png',
-	'STOCK_NEW' : 'new.png',
-	'STOCK_OPEN' : 'folder.png',
-	'STOCK_PREFERENCES' : 'configure.png',
-	'STOCK_PRINT' : 'print.png',
-	'STOCK_SELECT_COLOR' : 'colorpicker.png',
-	'STOCK_PRINT_PREVIEW' : 'print.png',
-	'terp-administration' : 'administration.png',
-	'terp-account' : 'account.png',
-	'terp-calendar': 'calendar.png',
-	'terp-camera_test': 'ok.png',
-	'terp-check' : 'ok.png',
-	'terp-crm' : 'crm.png',
-	'terp-dialog-close': 'close.png',
-	'terp-document-new' : 'new.png',
-	'terp-go-today' : 'go-today.png',
-	'terp-graph' : 'chart.png',
-	'terp-gtk-go-back-ltr' : 'previous.png',
-	'terp-gtk-go-back-rtl' : 'reload.png',
-	'terp-gtk-jump-to-ltr' : 'next.png',
-	'terp-gtk-jump-to-rtl' : 'previous.png',
-	'terp-gtk-media-pause' : 'pause.png',
-	'terp-hr' : 'hr.png',
-	'terp-mrp' : 'mrp.png',
-	'terp-partner' : 'partner.png',
-	'terp-personal' : 'partner.png',
-	'terp-product' : 'product.png',
-	'terp-project': 'clock.png',
-	'terp-accessories-archiver' : 'product.png',
-	'terp-purchase' : 'purchase.png',
-	'terp-sale' : 'sale.png',
-	'terp-stock' : 'stock.png',
-	'terp-stock_effects-object-colorize': 'view_tree.png',
-	'reload': 'reload.png'
+        'gtk-apply' : 'apply.png',
+        'gtk-cancel' : 'cancel.png',
+        'gtk-close' : 'close.png',
+        'gtk-convert' : 'convert.png',
+        'gtk-dialog-info' : 'info.png',
+        'gtk-edit' : 'edit.png',
+        'gtk-execute' : 'exec.png',
+        'gtk-find' : 'find.png',
+        'gtk-go-back' : 'previous.png',
+        'gtk-go-down' : 'down.png',
+        'gtk-go-forward' : 'next.png',
+        'gtk-go-up' : 'up.png',
+        'gtk-indent': 'indent.png',
+        'gtk-info' : 'info.png',
+        'gtk-jump-to' : 'relate.png',
+        'gtk-media-next' : 'media_next.png',
+        'gtk-media-pause' : 'pause.png',
+        'gtk-media-previous' : 'media_previous.png',
+        'gtk-new' : 'new.png',
+        'gtk-ok' : 'ok.png',
+        'gtk-open' : 'open.png',
+        'gtk-print' : 'print.png',
+        'gtk-save' : 'save.png',
+        'gtk-select-all' : 'text_block.png',
+        'gtk-sort-descending' : 'down.png',
+        'gtk-undo' : 'undo.png',
+        'gtk-yes' : 'ok.png',
+        'STOCK_CONVERT' : 'convert.png',
+        'STOCK_EXECUTE' : 'action.png',
+        'STOCK_INDENT' : 'rightjust.png',
+        'STOCK_JUSTIFY_FILL' : 'text_block.png',
+        'STOCK_NEW' : 'new.png',
+        'STOCK_OPEN' : 'folder.png',
+        'STOCK_PREFERENCES' : 'configure.png',
+        'STOCK_PRINT' : 'print.png',
+        'STOCK_SELECT_COLOR' : 'colorpicker.png',
+        'STOCK_PRINT_PREVIEW' : 'print.png',
+        'terp-administration' : 'administration.png',
+        'terp-account' : 'account.png',
+        'terp-calendar': 'calendar.png',
+        'terp-camera_test': 'ok.png',
+        'terp-check' : 'ok.png',
+        'terp-crm' : 'crm.png',
+        'terp-dialog-close': 'close.png',
+        'terp-document-new' : 'new.png',
+        'terp-go-today' : 'go-today.png',
+        'terp-graph' : 'chart.png',
+        'terp-gtk-go-back-ltr' : 'previous.png',
+        'terp-gtk-go-back-rtl' : 'reload.png',
+        'terp-gtk-jump-to-ltr' : 'next.png',
+        'terp-gtk-jump-to-rtl' : 'previous.png',
+        'terp-gtk-media-pause' : 'pause.png',
+        'terp-hr' : 'hr.png',
+        'terp-mrp' : 'mrp.png',
+        'terp-partner' : 'partner.png',
+        'terp-personal' : 'partner.png',
+        'terp-product' : 'product.png',
+        'terp-project': 'clock.png',
+        'terp-accessories-archiver' : 'product.png',
+        'terp-purchase' : 'purchase.png',
+        'terp-sale' : 'sale.png',
+        'terp-stock' : 'stock.png',
+        'terp-stock_effects-object-colorize': 'view_tree.png',
+        'reload': 'reload.png'
 }
 
-not_found = []
+_pixmapCache = {}
+
 log = logging.getLogger('Koo.Common.Icons')
 
-## @brief Returns a QIcon given an icon name. The name of the icon is usually a 
-# GTK or OpenERP name.
-def kdeIcon(icon):
-        if not icon:
-            pass
-        elif icon in not_found:
-            pass
-	elif icon in mapIcons:
-            return QIcon( ':/images/' + mapIcons[icon] )
-        else:
-            log.warning("KDE icon '%s' not found", icon)
-            not_found.append(icon)
 
-        return QIcon()
-
-## @brief Returns a QPixmap given an icon name. The name of the icon is usually 
-# a GTK or OpenERP name.
 def kdePixmap(icon):
-        if not icon:
-            pass
-        elif icon in not_found:
-            pass
-	elif icon in mapIcons:
-		return QPixmap( ':/images/' + mapIcons[icon] )
-	else:
-            log.warning("KDE icon '%s' not found", icon)
-            not_found.append(icon)
+        """ Returns a QPixmap given an icon name.
 
-        return QPixmap()
+            The name of the icon is usually a GTK or OpenERP name.
+        """
+        global _pixmapCache
+
+        if not icon:
+            return QPixmap()
+
+        if icon not in _pixmapCache:
+            log.debug('Attempting to load "%s" from image resources', icon)
+            # TODO: move mapping to resource file
+            # TODO: lazy-loading resources from OpenERP server
+            pix = None
+            if icon in mapIcons:
+                pix = QPixmap( ':/images/' + mapIcons[icon] )
+                # if pix.isNull():
+                #       log.debug("Image file %s not found",...)
+            else:
+                log.warning("KDE icon '%s' not found", icon)
+                pix = QPixmap()
+            _pixmapCache[icon] = pix
+
+        return _pixmapCache[icon]
+
+def kdeIcon(icon):
+        """ Returns a QIcon given an icon name.
+
+            The name of the icon is usually a GTK or OpenERP name.
+        """
+        return QIcon(kdePixmap(icon))
 
 # vim:noexpandtab:smartindent:tabstop=8:softtabstop=8:shiftwidth=8:
